66e0a0e219b3159887ec75f44fc003607091c984
uday-kiran-k <113407692+uday-kiran-k@users.noreply.github.com>
2023-02-23 15:21:16 -0500

    added sg grp and ec2

--- variables.tf
+++ variables.tf
@@ -8,5 +8,22 @@ variable "name_prefix" {
 }
 
 variable "vpc_cidr_block" {
-  # default = "10.0.0.0/16"
+  default = "10.0.0.0/16"
+}
+
+variable "security_cidr" {
+  default = "0.0.0.0/0"
+}
+variable "ami_id" {
+  # default = "ami-03081c6bdfb5e9f5d"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "key_name" {
+  default = "ec2"
+}
+variable "wsg_protocol" {
+  type    = string
+  default = "tcp"
 }
